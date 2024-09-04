0bc5763c2b133995678b6fd0c44c46c8241c86f3
johnny evans <evanseburu32@gmail.com>
2024-09-04 11:43:39 +0100

    initial commit

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(),
-    { cors: true }
+    { cors: corsConfig }
   );
   app.setGlobalPrefix("api");
   app.useGlobalPipes(

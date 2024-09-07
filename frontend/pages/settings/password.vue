d71be8d235ebf4a93f60438b84c5aa68124c9e5b
johnny evans <evanseburu32@gmail.com>
2024-09-07 05:17:45 +0100

    update

---
+++ frontend/pages/settings/password.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="border-b-[1px] border-base text-left py-5">
+    <CommonPageBar mainPage="Settings" currentPage="Password" />
+  </div>
+  <div class="flex flex-col gap-4 text-left">
+    <CommonFormInput
+      inputType="password"
+      v-model="oldPassword"
+      title="Enter your old password"
+      placeholder="password"
+    />
+    <CommonFormInput
+      inputType="password"
+      v-model="newPassword"
+      title="Enter your new password"
+      placeholder="password"
+      @keyup.enter="updatePassword"
+    />
+    <CommonButton
+      text="Update Password"
+      @btn-action="updatePassword"
+      custom-css="!bg-blue-600 w-full text-white"
+      :loading="loading"
+    />
+  </div>
+</template>
+
+<script lang="ts">
+import { notify } from "@kyvg/vue3-notification";
+import { useUserStore } from "~/store/user";
+
+export default defineComponent({
+  setup() {
+    useSeoMeta({
+      title: "Settings",
+      ogTitle: "Settings",
+    });
+    const { $api } = useNuxtApp();
+    const { setAccessToken } = useUserStore();
+    const oldPassword = ref();
+    const newPassword = ref();
+
+    const { withLoading, loading } = useLoading();
+
+    const updatePassword = async () => {
+      await withLoading(async () => {
+        await $api.userService.updatePassword({
+          newPassword: newPassword.value,
+          oldPassword: oldPassword.value,
+        });
+        setAccessToken(null);
+        await navigateTo("/signin");
+        notify({
+          type: "success",
+          title: "password updated",
+        });
+      });
+    };
+    return {
+      loading,
+      updatePassword,
+      oldPassword,
+      newPassword,
+    };
+  },
+});
+</script>

96431dc0a2e89d4be83a2034d57ae6f4583479bc
johnny evans <evanseburu32@gmail.com>
2024-09-15 01:34:44 +0100

    update

--- frontend/middleware/guard.global.ts
+++ frontend/middleware/guard.global.ts
@@ -4,9 +4,9 @@ import { useUserStore } from "~/store/user";
 export default defineNuxtRouteMiddleware(async (to) => {
   const { accessToken } = storeToRefs(useUserStore());
 
-  if (!to.meta.layout || to.meta.layout === "default") {
+  if (!to.meta?.layout || to.meta.layout === "default") {
     if (!accessToken.value) {
-      return navigateTo(`/signin?redirect=${to.fullPath}`);
+      return navigateTo(`/signin?redirect=${encodeURIComponent(to.fullPath)}`);
     }
   }
   if (to.meta.layout === "auth") {
